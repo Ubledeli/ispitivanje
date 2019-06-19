5b3e0007258cf6591f49dc38f08e96ffbd3ccec9
ubledeli <vodokotlic.vudukotlic@gmail.com>
2019-06-19 23:47:46 +0200

    data

---
+++ 6/sesta.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+u13 = [230, 180, 150]
+u23 = [240, 190, 180]
+i1 = [1, 0.88, 0.32]
+i2 = [1.5, 0.57, 0.48]
+a1 = [48, -20, 10]
+a2 = [-36, 21, -10]
+
+kw = 5
+
+u13ks = [12, 9, 5]
+u23ks = [13, 8.5, 6]
+i1ks = [6.8, 5, 2.6]
+i2ks = [6.8, 4.4, 3.4]
+a1ks = [9, 4, 4]
+a2ks = [-5, -9, -2]
+
+kw_prve_kolone = 10
+kw_druge_i_trece = 4
